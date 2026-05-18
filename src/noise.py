63f7a4cdfe44d04e8885eaabceac357b5dc20746
anrhy <henriroth77@gmail.com>
2026-05-18 22:01:21 +0300

    first tests with gaussian noise

--- src/noise.py
+++ src/noise.py
@@ -1,16 +1,12 @@
 import numpy as np
 
-def generate_signal(
-        n_points: int = 1000,
-        frequency: float = 1.0
-)-> np.ndarray:
-    x = np.linspace(0,10,n_points)
+
+def generate_signal(n_points: int = 1000, frequency: float = 1.0) -> np.ndarray:
+    x = np.linspace(0, 10, n_points)
     signal = np.sin(frequency * x)
     return signal
 
-def add_gaussian_noise(
-        signal: np.ndarray,
-        sigma: float = 0.2
-)-> np.ndarray:
-    noise = np.random.normal(0,sigma,size=signal.shape)
+
+def add_gaussian_noise(signal: np.ndarray, sigma: float = 0.2) -> np.ndarray:
+    noise = np.random.normal(0, sigma, size=signal.shape)
     return signal + noise
